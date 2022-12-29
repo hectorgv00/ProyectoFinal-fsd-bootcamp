20a71812d68a44b612ebfc01e4893fc636286632
hector.gv00 <hector.gv00@gmail.com>
2022-12-29 10:15:57 +0100

    [feature]: candidate login endpoint done

--- controllers/candidatesControllers.js
+++ controllers/candidatesControllers.js
@@ -1,6 +1,10 @@
 const models = require("../models/index");
 const jsonwebtoken = require("jsonwebtoken");
-const { assertValidPasswordService, assertEmailIsValid, encryptPassword } = require("../services/authServices");
+const {
+  assertValidPasswordService,
+  assertEmailIsValid,
+  encryptPassword,
+} = require("../services/authServices");
 const candidatesEndpoints = {};
 
 candidatesEndpoints.newCandidate = async (req, res) => {
@@ -18,29 +22,84 @@ candidatesEndpoints.newCandidate = async (req, res) => {
     }
 
     try {
-        assertEmailIsValid(email);
+      assertEmailIsValid(email);
     } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: `Email is invalid  ${error.message}`,
-          });
-          return
+      res.status(400).json({
+        success: false,
+        message: `Email is invalid  ${error.message}`,
+      });
+      return;
     }
 
     const hash = encryptPassword(password);
 
     await models.Candidates.create({
-        email: userBody.email,
-        password: hash,
-        name: userBody.name,
-        surname: userBody.surname,
-    })
+      email: userBody.email,
+      password: hash,
+      name: userBody.name,
+      surname: userBody.surname,
+    });
 
     res.status(200).json({
-        success: true,
-        message: `The candidate ${userBody.name} has been created successfully`,
+      success: true,
+      message: `The candidate ${userBody.name} has been created successfully`,
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+candidatesEndpoints.loginCandidate = async (req, res) => {
+  try {
+    let userBody = req.body;
+    let { password, email } = userBody;
+    const hashedPassword = encryptPassword(password);
+
+    const userFound = await models.Candidates.findOne({
+      where: { email: email },
+    });
+
+    if (!userFound) {
+      res
+        .status(401)
+        .json({
+          message: "Password or email is incorrect",
+        }); 
+      return;
+    }
+
+    if (hashedPassword !== userFound.password) {
+      res.status(401).json({ message: "Password or email is incorrect" });
+      return;
+    }
+
+    const secret = process.env.JWT_SECRET || "ghuireosghuios789345ghuljkdfgs";
+
+    if (secret.length < 10) {
+      res.status(400).json({
+        success: false,
+        message: "JWT Secret has not been set",
       });
-      return
+    }
+
+    const jwt = jsonwebtoken.sign(
+      {
+        id: userFound.id,
+        name: userFound.name,
+        email: userFound.email,
+        rol: "candidate",
+      },
+      secret
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      jwt: jwt,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
--- routes/candidatesRoutes.js
+++ routes/candidatesRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const candidatesEndpoints = require("../controllers/candidatesControllers");
 
 router.post("/register", candidatesEndpoints.newCandidate)
+router.post("/login", candidatesEndpoints.loginCandidate)
 
 module.exports = router;
