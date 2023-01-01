fa8aa3120d793904e333843ff01168c5f91e9718
hector.gv00 <hector.gv00@gmail.com>
2023-01-01 20:30:33 +0100

    [feat]: auth middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,40 @@
+const jsonwebtoken = require("jsonwebtoken");
+
+const authBearerMiddleware = async (req, res, next) => {
+    const { authorization } = req.headers;
+    const [strategy, jwt] = authorization.split(" ");
+    try {
+      if (strategy.toLowerCase() !== "bearer") {
+        throw new Error("Invalid strategy");
+      }
+      const payload = jsonwebtoken.verify(jwt, process.env.JWT_SECRET);
+      const created = payload.iat*1000;
+      const timeElapsed = Date.now() - created;
+      const MAX_TIME = Number(process.env.MAX_TIME_JWT_CADUCITY) ||
+        1000 * 60 * 60 * 24; // 1 day
+        const isValid = timeElapsed && created && MAX_TIME &&
+        (timeElapsed < MAX_TIME);
+      if (!isValid) {
+        throw new Error("Token expired");
+      }
+  
+      // expose the payload to the next middlewares and controllers
+      req.auth = payload;
+      next();
+  
+    } catch (error) {
+      res.status(401).json({ message: "You are not authenticated. Please log in again"});
+      return;
+    }
+  
+  };
+
+  const isValidRole = (role)  =>  (req, res, next) => {
+    if (req.auth?.role === role) {
+      next();
+    } else {
+      res.status(403).json({ message: "You are not authorized" });
+    }
+  }
+
+  module.exports = { authBearerMiddleware, isValidRole, };
